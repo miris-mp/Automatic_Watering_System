0b3c63f08dcec90eb182bfb072e7d15f29722670
miris-mp <miriam.petrocchi94@gmail.com>
2020-07-05 23:09:26 +0200

    add pump water

--- project/22_06_2020_Arduino/22_06_2020_Arduino.ino
+++ project/22_06_2020_Arduino/22_06_2020_Arduino.ino
@@ -1,28 +1,27 @@
-
-// Written by Nick Gammon
-// Date: 18th February 2011
-
 #include <Wire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2 // Data wire is plugged into D2 pin
+#define TEMPERATURE_PRECISION 9
 
-const byte MY_ADDRESS = 42;
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensor(&oneWire);
+DeviceAddress one;
 
+const byte MY_ADDRESS = 85;
+const int SOLENOID_VALVE_PIN = 8;
+//const int waterPumpPin = A3;
+int tempC;
 // various commands we might get
 enum {
   CMD_ID = 1,
-  CMD_READ_A0  = 2
+  CMD_READ_A0  = 2,
+  CMD_READ_D2 = 3,
+  CMD_TURN_ON_A2 = 4
 };
 
 
-// we have to take the values of the sensor for the calculation it's needed the airValue and the WaterValue as fix parameter
-const int AirValue = 620;   //you need to replace this value with Value_1
-const int WaterValue = 270;
-
-// generic value for the water to give
-int optimalMoistureValue = 600;
-
-// set the pin for the transistor
-int motorPin = 13;
-
+const int waterDuration = 60;
 char command;
 
 void setup()
@@ -30,7 +29,7 @@ void setup()
   command = 0;
 
   // set the output pin to turn on the transistor
-  pinMode(motorPin, OUTPUT);
+  pinMode(SOLENOID_VALVE_PIN, OUTPUT);
   /*
     pinMode (8, INPUT);
     digitalWrite (8, HIGH);  // enable pull-up
@@ -41,39 +40,131 @@ void setup()
   Wire.onReceive (receiveEvent);  // interrupt handler for incoming messages
   Wire.onRequest (requestEvent);  // interrupt handler for when data is wanted
 
+  pinMode(8, OUTPUT);
   Serial.begin(9600);
-}  // end of setup
+  //temperature
+  Serial.println("Dallas Temperature IC Control Library Demo");
+  sensor.begin();
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(sensor.getDeviceCount(), DEC);
+  Serial.println(" devices.");
+  Serial.print("Parasite power is: ");
+
+  if (sensor.isParasitePowerMode()) {
+    Serial.println("ON");
+  }
+  else {
+    Serial.println("OFF");
+  }
+  if (!sensor.getAddress(one, 0)) {
+    Serial.println("Unable to find address for Device 0");
+  }
 
 
+  Serial.print("Device 0 Address: ");
+  printAddress(one);
+  Serial.println(); Serial.print("Device 1 Address: ");
+
+  sensor.setResolution(one, TEMPERATURE_PRECISION);
+
+
+  // one tab
+  Serial.print("Device 0 Resolution: ");
+  Serial.print(sensor.getResolution(one), DEC);
+  Serial.println();
+}
+int temp;
 void loop()
 {
+  Serial.print("Requesting temperatures...");
+  sensor.requestTemperatures();
+  Serial.println("DONE");
+  printData(one);
+
+  delay(1000);
   // all done by interrupts
 }  // end of loop
 
 void receiveEvent (int howMany)
 {
-  // remember command for when we get request
+  Serial.println("receiveEvent");
+  // save the command send by NodeMCU
   command = Wire.read ();
+  Serial.println("receiveEventEnd");
 } // end of receiveEvent
 
 
-void sendSensor (const byte analogValueRead)
+void sendSensor (const byte analogValueRead, const int num)
 {
   int value = analogRead(analogValueRead);
   byte buf [2];
-
   buf [0] = value >> 8;
   buf [1] = value & 0xFF;
-  Wire.write (buf, 2);
+  Wire.write(buf,sizeof(buf));
 }  // end of sendSensor
 
+void temptWrite (const int num) {
+    byte buf [2];
+  buf [0] = num ;
+
+  Wire.write (buf, 1);
+}
 void requestEvent ()
 {
+
   switch (command)
   {
-    case CMD_ID:      Wire.write (0x55); break;   // send our ID
-    case CMD_READ_A0: sendSensor (A0); break;  // send moisture value
-      //case CMD_READ_A1: sendSensor (A1);  // send temperature value
+    case CMD_ID:
+      Wire.write (0x55);
+      break;
+    case CMD_READ_A0:
+      sendSensor (A0,tempC);
+      break;  // send moisture value
+    case CMD_READ_D2:
+    temptWrite(tempC);
+    Serial.println("SII");
+      Serial.println(tempC);
+      break;
+    // send temperature value
+    case CMD_TURN_ON_A2:
+      solenoidValveOn();
+      delay(waterDuration);
+      solenoidValveOff();
+
+      // we have to turn on the solenoid valve
+      break;
   }  // end of switch
 
 }  // end of requestEvent
+
+void solenoidValveOn() {
+  digitalWrite(SOLENOID_VALVE_PIN, HIGH);
+}
+
+void solenoidValveOff() {
+  digitalWrite(SOLENOID_VALVE_PIN, LOW);
+}
+
+
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++) {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+void printTemperature(DeviceAddress deviceAddress) {
+  tempC = sensor.getTempC(deviceAddress);
+  Serial.print("Temp: "); Serial.print(tempC);
+  Serial.print(" C; ");
+  Serial.print(DallasTemperature::toFahrenheit(tempC));
+  Serial.print(" F");
+}
+void printResolution(DeviceAddress deviceAddress) {
+  Serial.print("Resolution: ");
+  Serial.println(sensor.getResolution(deviceAddress));
+}
+void printData(DeviceAddress deviceAddress) {
+  printTemperature(deviceAddress); Serial.print(" ;  Device: ");
+  printAddress(deviceAddress); Serial.println();
+}
--- project/22_06_2020_NodeMcu/22_06_2020_NodeMcu.ino
+++ project/22_06_2020_NodeMcu/22_06_2020_NodeMcu.ino
@@ -10,11 +10,8 @@
 // wifi access
 char* ssid = "Redmi";
 char* password = "p4ssw0rd";
-// Domain Name with full URL Path for HTTP POST Request
-const char* serverName = "http://api.thingspeak.com/update";
-// Service API Key
-String apiKey = "UXYEVVEWKC4DAGIP";  
-//char* serverName = "http://miris.pythonanywhere.com/postjson";
+
+char* serverName = "http://miris.pythonanywhere.com/postjson";
 
 
 //The udp library class
@@ -35,6 +32,11 @@ int currentYear = 0;
 const int AirValue = 620;   //you need to replace this value with Value_1
 const int WaterValue = 270;
 
+// the optimal water value is checked with the percentage one
+const int optimalWaterValue = 20;
+const int dry = 520;
+const int wet = 270;
+
 int soilMoistureValue = 0;
 int soilMoisturePercentage = 0;
 int soilTemperatureValue = 0;
@@ -45,15 +47,16 @@ const int SLAVE_ADDRESS = 42;
 // various commands we might send
 enum {
   CMD_ID = 1,
-  CMD_READ_A0  = 2
+  CMD_READ_A0  = 2,
+  CMD_TURN_ON_A2 = 3
   //CMD_READ_D8 = 3
 };
 
 //Telegram connection
 /*
-CTBot myBot;
-String token = "1176180431:AAEQfRsYrJMLfx3QDsC3h--WqL3xnUQpdBc";   // REPLACE myToken WITH YOUR TELEGRAM BOT TOKEN
-uint8_t led = 2;            // the onboard ESP8266 LED.
+  CTBot myBot;
+  String token = "1176180431:AAEQfRsYrJMLfx3QDsC3h--WqL3xnUQpdBc";   // REPLACE myToken WITH YOUR TELEGRAM BOT TOKEN
+  uint8_t led = 2;            // the onboard ESP8266 LED.
 */
 void sendCommand (const byte cmd, const int responseSize)
 {
@@ -90,22 +93,23 @@ void setup ()
   // time
   timeClient.begin();
   timeClient.setTimeOffset(7200);
-/*
-  // connect the ESP8266 to the desired access point
-  myBot.wifiConnect(ssid, password);
 
-  // set the telegram bot token
-  myBot.setTelegramToken(token);
+  /*
+    // connect the ESP8266 to the desired access point
+    myBot.wifiConnect(ssid, password);
 
-  // check if all things are ok
-  if (myBot.testConnection())
-    Serial.println("\ntestConnection OK");
-  else
-    Serial.println("\ntestConnection NOK");
+    // set the telegram bot token
+    myBot.setTelegramToken(token);
 
-  // set the pin connected to the LED to act as output pin
-  pinMode(led, OUTPUT);
-  digitalWrite(led, HIGH); // turn off the led (inverted logic!)
+    // check if all things are ok
+    if (myBot.testConnection())
+      Serial.println("\ntestConnection OK");
+    else
+      Serial.println("\ntestConnection NOK");
+
+    // set the pin connected to the LED to act as output pin
+    pinMode(led, OUTPUT);
+    digitalWrite(led, HIGH); // turn off the led (inverted logic!)
   */
 }  // end of setup
 
@@ -121,18 +125,17 @@ void loop()
   soilMoistureValue |= Wire.read ();
   Serial.print ("Value of A0: ");
   Serial.println (soilMoistureValue, DEC);
-  
+
+  // calculate moisture percentage
   soilMoisturePercentage = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
-  
 
   //sendCommand (CMD_READ_D8, 3);
   /*
-  val = Wire.read ();
-  Serial.print ("Value of D8: ");
-  Serial.println (val, DEC);
+    val = Wire.read ();
+    Serial.print ("Value of D8: ");
+    Serial.println (val, DEC);
   */
 
-  delay (500);
   // check WiFi connection
   if (WiFi.status() == WL_CONNECTED) {
     //calling function for request and save date and time
@@ -148,19 +151,8 @@ void loop()
     //Declare object of class HTTPClient
     HTTPClient http;
     http.begin(serverName);
-/*
+
     http.addHeader("Content-Type", "application/json");
-    int httpCode = http.POST(String(databuf));
-    String payload = http.getString();
-    */
-    //content-type header
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-     // Data to send with HTTP POST
-      String httpRequestData = "api_key=" + apiKey + "&field1=" + String(soilMoistureValue) + "&field2=" + String(soilMoisturePercentage);       
-      
-      // Send HTTP POST request
-      int httpResponseCode = http.POST(httpRequestData);
-      
     int httpCode = http.POST(String(databuf));
     String payload = http.getString();
 
@@ -170,27 +162,47 @@ void loop()
     http.end();
   }
 
-  // a variable to store telegram message data
- /* TBMessage msg;
+  Serial.println("acqus ");
+  Serial.println(hour);
+  if (hour > 22) {
+    // we have to give the water
+    if (soilMoisturePercentage < optimalWaterValue) {
+      // tell to Arduino to turn on the valve
+      sendCommand(CMD_TURN_ON_A2, 3);
 
-  // if there is an incoming message...
-  if (myBot.getNewMessage(msg)) {
-
-    if (msg.text.equalsIgnoreCase("LIGHT ON")) {              // if the received message is "LIGHT ON"...
-      digitalWrite(led, LOW);                               // turn on the LED (inverted logic!)
-      myBot.sendMessage(msg.sender.id, "Light is now ON");  // notify the sender
+    } else {
+      Serial.println("The water level are good");
     }
-    else if (msg.text.equalsIgnoreCase("LIGHT OFF")) {        // if the received message is "LIGHT OFF"...
-      digitalWrite(led, HIGH);                              // turn off the led (inverted logic!)
-      myBot.sendMessage(msg.sender.id, "Light is now OFF"); // notify the sender
-    }
-    else {                                                    // otherwise...
-      // generate the message for the sender
-      String reply;
-      reply = (String)"Welcome " + msg.sender.username + (String)". Try LIGHT ON or LIGHT OFF.";
-      myBot.sendMessage(msg.sender.id, reply);             // and send it
+  } else {
+        if (soilMoisturePercentage < optimalWaterValue) {
+      // tell to Arduino to turn on the valve
+      sendCommand(CMD_TURN_ON_A2, 3);
+    } else {
+      Serial.println("The water level are good");
     }
   }
+
+  // a variable to store telegram message data
+  /* TBMessage msg;
+
+    // if there is an incoming message...
+    if (myBot.getNewMessage(msg)) {
+
+     if (msg.text.equalsIgnoreCase("LIGHT ON")) {              // if the received message is "LIGHT ON"...
+       digitalWrite(led, LOW);                               // turn on the LED (inverted logic!)
+       myBot.sendMessage(msg.sender.id, "Light is now ON");  // notify the sender
+     }
+     else if (msg.text.equalsIgnoreCase("LIGHT OFF")) {        // if the received message is "LIGHT OFF"...
+       digitalWrite(led, HIGH);                              // turn off the led (inverted logic!)
+       myBot.sendMessage(msg.sender.id, "Light is now OFF"); // notify the sender
+     }
+     else {                                                    // otherwise...
+       // generate the message for the sender
+       String reply;
+       reply = (String)"Welcome " + msg.sender.username + (String)". Try LIGHT ON or LIGHT OFF.";
+       myBot.sendMessage(msg.sender.id, reply);             // and send it
+     }
+    }
   */
   // wait 500 milliseconds
   delay(500);
