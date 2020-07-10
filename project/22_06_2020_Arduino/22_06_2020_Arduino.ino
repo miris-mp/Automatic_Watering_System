3e1668ea51f87a95de6a6492602d52453e1fe085
miris-mp <59831666+miris-mp@users.noreply.github.com>
2020-07-10 05:51:50 +0300

    Update 22_06_2020_Arduino.ino
    checked comments

--- project/22_06_2020_Arduino/22_06_2020_Arduino.ino
+++ project/22_06_2020_Arduino/22_06_2020_Arduino.ino
@@ -8,7 +8,7 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature temp(&oneWire);
 DeviceAddress one;
 
-const byte MY_ADDRESS = 85;
+const byte MY_ADDRESS = 42;
 const int SOLENOID_VALVE_PIN = 8;
 //const int waterPumpPin = A3;
 int tempC;
@@ -17,11 +17,9 @@ enum {
   CMD_ID = 1,
   CMD_READ_A0  = 2,
   CMD_READ_D2 = 3,
-  CMD_TURN_ON_A2 = 4,
-  CMD_ON = 5,
-  CMD_OFF = 6
+  CMD_ON = 4,
+  CMD_OFF = 5
 };
-
 
 const int waterDuration = 5000;
 char command;
@@ -32,12 +30,7 @@ void setup()
 
   // set the output pin to turn on the transistor
   pinMode(SOLENOID_VALVE_PIN, OUTPUT);
-  /*
-    pinMode (8, INPUT);
-    digitalWrite (8, HIGH);  // enable pull-up
-    pinMode (A0, INPUT);
-    digitalWrite (A0, LOW);  // disable pull-up
-  */
+
   Wire.begin (MY_ADDRESS);
   Wire.onReceive (receiveEvent);  // interrupt handler for incoming messages
   Wire.onRequest (requestEvent);  // interrupt handler for when data is wanted
@@ -46,7 +39,7 @@ void setup()
   Serial.begin(9600);
   //temperature
   temp.begin();
-  Serial.print(temp.getDeviceCount(), DEC);
+  Serial.println(temp.getDeviceCount(), DEC);
 
   if (temp.isParasitePowerMode()) {
     Serial.println("ON");
@@ -58,42 +51,37 @@ void setup()
     Serial.println("Unable to find address for Device 0");
   }
 
-
   printAddress(one);
 
   temp.setResolution(one, TEMPERATURE_PRECISION);
 
-
   // one tab
   Serial.print(temp.getResolution(one), DEC);
+
+  
 }
 
 void loop()
 {
-  temp.requestTemperatures();
   printData(one);
-
-  delay(1000);
+  temp.requestTemperatures();
   // all done by interrupts
-}  // end of loop
+}
 
 void receiveEvent (int howMany)
 {
   // save the command send by NodeMCU
   command = Wire.read ();
 
-switch (command)
+  switch (command)
   {
     case CMD_ON:
-    Serial.println("muscoli");
-    solenoidValveOn();
-    break;
+      solenoidValveOn();
+      break;
     case CMD_OFF:
-    solenoidValveOff();
-    break;
-    
-    }
-
+      solenoidValveOff();
+      break;
+  }
 } // end of receiveEvent
 
 
@@ -109,12 +97,10 @@ void sendSensor(const byte analogValueRead, const int num)
 void temptWrite (const int num) {
   byte buf [2];
   buf [0] = num ;
-
   Wire.write (buf, 1);
 }
 void requestEvent ()
 {
-
   switch (command)
   {
     case CMD_ID:
@@ -125,15 +111,6 @@ void requestEvent ()
       break;  // send moisture value
     case CMD_READ_D2:
       temptWrite(tempC);
-      Serial.println(tempC);
-      break;
-    // send temperature value
-    case CMD_TURN_ON_A2:
-      solenoidValveOn();
-      delay(waterDuration);
-      solenoidValveOff();
-
-      // we have to turn on the solenoid valve
       break;
   }  // end of switch
 
@@ -147,11 +124,10 @@ void solenoidValveOff() {
   digitalWrite(SOLENOID_VALVE_PIN, LOW);
 }
 
-
 void printAddress(DeviceAddress deviceAddress) {
   for (uint8_t i = 0; i < 8; i++) {
     if (deviceAddress[i] < 16) Serial.print("0");
-    //Serial.print(deviceAddress[i], HEX);
+    Serial.print(deviceAddress[i], HEX);
   }
 }
 
